81a5a34766f3a26269b6a91def2f2934def4c970
carlpielou03 <tul35916@temple.edu>
2026-04-19 10:50:21 -0400

    Another Test to see if the collection name
    Let's see if we get the collection name,

--- src/commands/check_vibes_command.py
+++ src/commands/check_vibes_command.py
@@ -14,21 +14,23 @@ def register_check_vibes_command(bolt_app, state_manager):
     installations_col = db["installations"]
 
     @bolt_app.command("/checkvibes")
-    def handle_checkvibes(ack, respond, body):
+    def handle_checkvibes(ack, respond, body, client):
         ack()
         respond("STARTING TO Check the Vibes!!")
         channel = body.get("channel_id")  # default to the channel where command was used
         team_id = body.get("team_id") or (body.get("authorizations") or [{}])[0].get("team_id") or ""
-        respond("GOT THE VIBES OF THE CHANNEL")
         team_name = None
         if team_id:
-            respond("vibe of the team grabbed.")
             record = installations_col.find_one({"team_id": team_id})
             if record:
                 respond("Vibes of the records grabbed")
                 team_name = record.get("team_name")
+            else:
+                respond("Could not find the record")
+        
         respond("GOT THE VIBES OF RECORD, CHECKING COLLECTION NAME!")
         collection_name = f"messages_{team_name}" if team_name else f"messages_{team_id or 'unknown'}"
+        respond(f"Grabbing the messages of {collection_name}")
         messages_col = db[collection_name]
         if not messages_col:
             respond(f"This channel does not have any logs!")

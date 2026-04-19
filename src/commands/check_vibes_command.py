89bcb052355a960bc7d121ad06315bd09dcfd7c8
carlpielou03 <tul35916@temple.edu>
2026-04-19 11:26:01 -0400

    Made the Command more asyncronous
    testing the asycnhrounos.

--- src/commands/check_vibes_command.py
+++ src/commands/check_vibes_command.py
@@ -1,6 +1,6 @@
 # src/commands/check_vibes_command.py
 import logging
-import random
+import threading
 import os
 
 from pymongo import MongoClient
@@ -8,45 +8,45 @@ from services.prompt_service import get_random_prompt_text, mark_prompt_asked
 from services.mongo_service import get_tracker
 
 
-def register_check_vibes_command(bolt_app, state_manager):
-    mongo_client = MongoClient(os.getenv("MONGO_URI"))
+def databse_Task(mongo_client, payload, respond):
     db = mongo_client.get_database("vibecheck")
     installations_col = db.get_collection("installations")
+    channel = payload.get("channel_id")  # default to the channel where command was used
+    team_id = payload.get("team_id") or (payload.get("authorizations") or [{}])[0].get("team_id") or ""
+    team_name = None
+    if team_id:
+        record = installations_col.find_one({"team_id": team_id})
+        if record:
+            respond("Vibes of the records grabbed")
+            team_name = record.get("team_name")
+        else:
+            respond("Could not find the record")
+    
+    collection_name = f"messages_{team_name}" if team_name else f"messages_{team_id or 'unknown'}"
+
+    if(not db):
+        respond(f"There isn't a databse called vibecheck.")
+    else:
+        respond(f"There is a databse called vibecheck.")
+
+    messages_col = db.get_collection(collection_name)
+    if not messages_col:
+        respond(f"This channel does not have any logs!")
+        return
+    else:
+        respond(f"This channel does have logs!")
+
+    respond("Checking the Vibes!!")
+
+def register_check_vibes_command(bolt_app, state_manager):
+    mongo_client = MongoClient(os.getenv("MONGO_URI"))
 
     @bolt_app.command("/checkvibes")
     def handle_checkvibes(ack, respond, body, client):
-        ack()
-        if(db):
-            respond(f"Database has been grabbed. {db.list_collection_names}")
-
-
-        respond("STARTING TO Check the Vibes!!")
-        channel = body.get("channel_id")  # default to the channel where command was used
-        team_id = body.get("team_id") or (body.get("authorizations") or [{}])[0].get("team_id") or ""
-        team_name = None
-        if team_id:
-            record = installations_col.find_one({"team_id": team_id})
-            if record:
-                respond("Vibes of the records grabbed")
-                team_name = record.get("team_name")
-            else:
-                respond("Could not find the record")
+        ack("Checking out the vibes...")
         
-        collection_name = f"messages_{team_name}" if team_name else f"messages_{team_id or 'unknown'}"
-
-        if(not db):
-            respond(f"There isn't a databse called vibecheck.")
-        else:
-            respond(f"There is a databse called vibecheck.")
-
-        messages_col = db.get_collection(collection_name)
-        if not messages_col:
-            respond(f"This channel does not have any logs!")
-            return
-        else:
-            respond(f"This channel does have logs!")
-
-        respond("Checking the Vibes!!")
+        threading.Thread(target=databse_Task, args=(mongo_client, body, respond)).start()
+        respond("Checking out the Vibes!!")
         # for message in messages_col.find():
         #    message.get()
 

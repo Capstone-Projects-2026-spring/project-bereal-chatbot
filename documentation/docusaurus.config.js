622d42c9c73b22ab3c155fd059e663ab02384f7e
Justin Pham <justphams@gmail.com>
2026-04-25 13:42:39 -0400

    added logo to documentation

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -8,7 +8,7 @@
  * The URL or reference to your projects logo!
  * @type {string}
  */
-const logo = 'https://upload.wikimedia.org/wikipedia/commons/1/17/Temple_T_logo.svg';
+const logo = 'https://imgur.com/a/u8vVkCb';
 
 const main_template_jira_scripts = () => {
   if (process.env.ORG_NAME === 'ApplebaumIan'){
